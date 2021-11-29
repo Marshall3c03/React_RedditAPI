3eb45b3d62e9892c07df54ac865ce8d42160077f
Craig Marshall <dev.craigmarshall@gmail.com>
2021-11-29 09:58:00 +0000

    made the post appear as the latest post on the page

--- src/containers/RedditContentContainer.js
+++ src/containers/RedditContentContainer.js
@@ -6,10 +6,26 @@ import './RedditContentContainer.css'
 const RedditContentContainer = ({ categories }) => {
     
     const [posts, setPosts] = useState([])
+    const [subRedditTitle, setSubRedditTitle] = useState([])
+    const [subRedditAvatar, setSubRedditAvatar] = useState([])
 
     useEffect(() => {
         getPosts();
     }, [])
+
+    useEffect(() => {
+        getSubRedditTitle();
+    }, [])
+
+    useEffect(() => {
+        getSubRedditAvatar();
+    }, [])
+
+    useEffect(() => {
+        modifyAvatarLink();
+    }, [])
+
+
 
     const getPosts = (()=>{
         fetch('https://www.reddit.com/r/meme/new/.json')
@@ -19,9 +35,33 @@ const RedditContentContainer = ({ categories }) => {
         console.log("Loaded Posts")
     })
 
+    const getSubRedditTitle = (()=>{
+        fetch('https://www.reddit.com/r/gaming/about/.json')
+        .then(response => response.json())
+        .then(response => response.data.title)
+        .then(subRedditTitle => setSubRedditTitle(subRedditTitle))
+
+        // console.log(subRedditTitle)
+        console.log("Loaded Posts")
+    })
+
+    const getSubRedditAvatar = (()=>{
+        fetch('https://www.reddit.com/r/gaming/about/.json')
+        .then(response => response.json())
+        .then(response => response.data.community_icon)
+        .then(subRedditAvatar => setSubRedditAvatar(subRedditAvatar))
+
+        // console.log(subRedditAvatar)
+    })
+
+    const modifyAvatarLink = () => {
+        console.log(subRedditAvatar)
+        // return subRedditAvatar.replace('amp;', '')
+    }
+
     const addPost = (submittedPost) => {
         submittedPost.id = Date.now();
-        const updatedPosts = [...posts, submittedPost];
+        const updatedPosts = [submittedPost,...posts];
         setPosts(updatedPosts);
     }
     
